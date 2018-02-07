99e5b40f7c1088a31a7702cb851127b8dd6ed0f1
Juan P. Ruiz <juan.ruiz@taiosystems.com>
2018-02-07 10:37:26 -0500

    analizeTopic process now also top resutls

--- analizeTopic.py
+++ analizeTopic.py
@@ -1,12 +1,10 @@
-import csv
 import paperUtils
 import paperSave
 import globalVar
 import os
-import sys
 import matplotlib.pyplot as plt
-import math
-import re
+import numpy as np
+import graphUtils
 
 
 import argparse
@@ -15,6 +13,11 @@ parser = argparse.ArgumentParser(description="Analyze the topics inside a criter
 parser.add_argument("criterion", choices=["authors", "source",  "subject",
 "authorKeywords", "indexKeywords", "documentType", "dataBase", "country"], 
 help="Select the criterion to analyze the topics")
+
+parser.add_argument("-l", "--length", type=int, default=10, help="Length of the top topics to present, default 10")
+
+parser.add_argument("-s", "--start", type=int, default=0,  help="To filter the \
+first elements, ex to filter the first 2 elements on the list use -s 2")
 
 parser.add_argument("-t", "--topics", help='Topics to analyze according to critera, '
                                            'ex: authorKeywords -t "internet of things,iot;bluetooth" ')
@@ -36,11 +39,20 @@ help="Do not plot the results, use for large amount of topics", action="store_fa
 parser.add_argument("--parametric",
 help="Graph on Y the number of publications, and on X accomulative number of citatiosn", action="store_true")
 
+parser.add_argument("--useCitedBy",
+help="Short the top results based on times cited", action="store_true")
+
+parser.add_argument("-r", "--previousResults",
+help="Analyze based on the previous results", action="store_false")
+
+
 
 # Program start ********************************************************
+print("\n\nScientoPy: %s" % (globalVar.SCIENTOPY_VERSION))
+print("================\n")
+
 
 args = parser.parse_args()
-
 
 # Create output folders if not exist
 if not os.path.exists(globalVar.GRAPHS_OUT_FOLDER):
@@ -48,28 +60,11 @@ if not os.path.exists(globalVar.GRAPHS_OUT_FOLDER):
 if not os.path.exists(globalVar.RESULTS_FOLDER):
     os.makedirs(globalVar.RESULTS_FOLDER)
 
-# Divide the topics by ;
-topicsFirst = args.topics.split(";")
-
-topicList = []
-for x in topicsFirst:
-  topicList.append(x.split(","))
-
-# Remove begining space from topics
-for topic in topicList:
-  for idx,item in enumerate(topic):
-    topic[idx] = item.strip()
-
-
-print topicList
-
-# Remove input start and end spaces 
-for item1 in topicList:
-  for item2 in item1:
-    item2 = item2.strip()
-
-INPUT_FILE = os.path.join(globalVar.DATA_OUT_FOLDER, globalVar.OUTPUT_FILE_NAME)
-
+# Select the input file
+if args.previousResults:
+  INPUT_FILE = os.path.join(globalVar.DATA_OUT_FOLDER, globalVar.OUTPUT_FILE_NAME)
+else:
+  INPUT_FILE = os.path.join(globalVar.RESULTS_FOLDER, globalVar.OUTPUT_FILE_NAME)
 
 # Start paper list empty
 papersDict = []
@@ -91,21 +86,84 @@ yearArray = range(args.startYear, args.endYear + 1)
 yearPapers = {}
 for i in range(args.startYear, args.endYear + 1):
   yearPapers[i] = 0
-  
+
+# Filter the papers outside the year range
+papersDict = list(filter(lambda x: int(x["year"]) >= args.startYear, papersDict))
+papersDict = list(filter(lambda x: int(x["year"]) <= args.endYear, papersDict))
+
+print("Total papers in range (%s - %s): %s" %
+      (args.startYear, args.endYear , len(papersDict)))
+
 # Find the number of total papers per year
 for paper in papersDict:
   if int(paper["year"]) in yearPapers.keys():
-    yearPapers[int(paper["year"])] += 1 
-    
-# Create results data dictionary
-topicResults = {}
+    yearPapers[int(paper["year"])] += 1
 
+topicList = []
+# Parse custom topics
+if args.topics:
+  print("Custom topics entered:")
+
+  # Divide the topics by ;
+  topicsFirst = args.topics.split(";")
+
+  for x in topicsFirst:
+    topicList.append(x.split(","))
+
+  # Remove begining space from topics
+  for topic in topicList:
+    for idx,item in enumerate(topic):
+      topic[idx] = item.strip()
+
+  # Remove for each sub topic, start and end spaces
+  for item1 in topicList:
+    for item2 in item1:
+      item2 = item2.strip()
+
+# Find the top topics
+else:
+  print("Finding the top topics...")
+
+  topicDic = {}
+  # Find papers within the arguments
+  # run on papersDict
+  for paper in papersDict:
+    for item in paper[args.criterion].split(";"):
+      item = item.strip()
+      item = item.upper()
+      if item == "":
+        continue
+      try:
+        if item in topicDic:
+          if not args.useCitedBy:
+            topicDic[item] += 1
+          else:
+            topicDic[item] += int(paper["citedBy"])
+        else:
+          if not args.useCitedBy:
+            topicDic[item] = 1
+          else:
+            topicDic[item] = int(paper["citedBy"])
+      except:
+        noWithCitedBy = 1
+
+  topTopcis = sorted(topicDic.iteritems(),
+                     key=lambda x: -x[1])[int(args.start):int(args.length)]
+
+  for topic in topTopcis:
+    topicList.append([topic[0]])
+
+print topicList
+
+# Create results data dictionary and init fields
+topicResults = {}
 for topics in topicList:
   topicName = topics[0].upper()
   topicResults[topicName] = {}
   topicResults[topicName]["year"] = yearArray
   topicResults[topicName]["PapersCount"] = [0] * len(yearArray)
   topicResults[topicName]["PapersCountAccum"] = [0] * len(yearArray)
+  topicResults[topicName]["PapersCountRate"] = [0] * len(yearArray)
   topicResults[topicName]["PapersTotal"] = 0
   topicResults[topicName]["CitedByCount"] = [0] * len(yearArray)
   topicResults[topicName]["CitedByCountAccum"] = [0] * len(yearArray)
@@ -113,36 +171,50 @@ for topics in topicList:
   topicResults[topicName]["name"] = topics[0]
   topicResults[topicName]["papers"] = []
   topicResults[topicName]["hIndex"] = []
-  
+  topicResults[topicName]["agr"] = 0
 #print(topicResults)
 
-# Find papers within the arguments, and get the PapersCount per year.
+# Find papers within the arguments, and fill the topicResults fileds per year.
 # run on papersDict
-noIncludedInRange = 0
 for paper in papersDict:
-  # run on input arguments
-  for item in paper[args.criterion].upper().split("; "):
+  # run on criterin sub fields
+  for item in paper[args.criterion].split(";"):
+    item = item.strip()
+    itemUp = item.upper()
+
+    # run in topicList to fill topicResults
     for topics in topicList:
       for topic in topics:
-        if topic.upper() == item.upper(): 
-          try:
-            index = topicResults[topics[0].upper()]["year"].index(int(paper["year"]))
-            topicResults[topics[0].upper()]["PapersCount"][index] += 1
-            topicResults[topics[0].upper()]["PapersTotal"] += 1
-            topicResults[topics[0].upper()]["CitedByCount"][index] += int(paper["citedBy"])
-            topicResults[topics[0].upper()]["CitedByTotal"] += int(paper["citedBy"])
-            topicResults[topics[0].upper()]["name"] = item
-            topicResults[topics[0].upper()]["papers"].append(paper)
-            papersDictOut.append(paper)
-          except:
-            noIncludedInRange += 1
-            #print("Paper on year: %s" % paper.year)
+        if topic.upper() == itemUp:
+          topicName = topics[0].upper()
+          yearIndex = topicResults[topicName]["year"].index(int(paper["year"]))
+          topicResults[topicName]["PapersCount"][yearIndex] += 1
+          topicResults[topicName]["PapersTotal"] += 1
+          topicResults[topicName]["CitedByCount"][yearIndex] += int(paper["citedBy"])
+          topicResults[topicName]["CitedByTotal"] += int(paper["citedBy"])
+          topicResults[topicName]["name"] = item
+          topicResults[topicName]["papers"].append(paper)
+          papersDictOut.append(paper)
 
 #print(topicResults)
 
 
+# Extract the Average Growth Rate (AGR)
+for topic in topicList:
+  topicName = topic[0].upper()
+  citedAccumValue = 0
+  papersAccumValue = 0
 
+  # Calculate rates
+  pastCount = 0
+  for i in range(0, len(topicResults[topicName]["PapersCount"])):
+    topicResults[topicName]["PapersCountRate"][i] = topicResults[topicName]["PapersCount"][i] - pastCount
+    pastCount = topicResults[topicName]["PapersCount"][i]
 
+  # Calculate AGR from rates
+  endYearIndex = len(topicResults[topicName]["PapersCount"])
+  startYearIndex = endYearIndex - 2
+  topicResults[topicName]["agr"] = np.mean(topicResults[topicName]["PapersCountRate"][startYearIndex:endYearIndex])
 
 # Extract citedby accumulative
 for topic in topicList:
@@ -156,7 +228,7 @@ for topic in topicList:
     papersAccumValue += topicResults[topicName]["PapersCount"][i]
     topicResults[topicName]["PapersCountAccum"][i] = papersAccumValue
 
-
+# Scale in percentage per year
 if args.pYear:
   for topics in topicList:
     for year, value in yearPapers.iteritems():
@@ -165,7 +237,7 @@ if args.pYear:
         topicResults[topics[0].upper()]["PapersCount"][index] /= (float(value)/100.0)
 
 
-# h index **********************
+# Calculate h index per topic
 for topic in topicList:
   topicName = topic[0].upper()
   #print("\n" + topicName)
@@ -181,15 +253,17 @@ for topic in topicList:
     if int(paper["citedBy"]) >= count:
       hIndex = count
     count += 1
-
-  #print("hIndex: " + str(hIndex))
+    #print("hIndex: " + str(hIndex))
     topicResults[topicName]["hIndex"] = hIndex
 
-print("\nTop list:")
+# Print top topics
+print("\nTop topics:")
+print("Pos. " + args.criterion + ", Total, h-index")
 count = 0
-for topics in topicList:
-  print("%s. %s: %s" % (count + 1, 
-  str(topics).translate(None, "'[]'"), topicResults[topics[0].upper()]["PapersTotal"]))
+for topic in topTopcis:
+  print("%s. %s: %s, %s" % (count + 1,
+  topicResults[topic[0].upper()]["name"], topicResults[topic[0].upper()]["PapersTotal"],
+                            str(topicResults[topic[0].upper()]["hIndex"])))
   count += 1
 
 # Plot
@@ -198,18 +272,30 @@ if args.noPlot:
   if args.parametric:
     count = 0
     legendArray = []
+    dataPlot = []
     for topics in topicList:
-      legendArray.append(topics[0])
+      legendArray.append(topicResults[topics[0].upper()]["name"])
 
-      plt.plot(topicResults[topics[0].upper()]["CitedByCountAccum"], topicResults[topics[0].upper()]["PapersCountAccum"],
-               linewidth=1.2, marker=globalVar.MARKERS[count], markersize=10,
-               zorder=(len(topicList) - count), color=globalVar.COLORS[count], markeredgewidth=0.0)
+      dataPlot.append([topicResults[topics[0].upper()]["CitedByTotal"],
+                       topicResults[topics[0].upper()]["agr"],
+                       topicResults[topics[0].upper()]["PapersTotal"]
+                       ])
+
+      #plt.plot(topicResults[topics[0].upper()]["CitedByTotal"], topicResults[topics[0].upper()]["agr"],
+      #         linewidth=1.2, marker=globalVar.MARKERS[count], markersize=10,
+      #         zorder=(len(topicList) - count), color=globalVar.COLORS[count], markeredgewidth=0.0)
 
       count += 1
 
-    plt.legend(legendArray, loc=2)
-    plt.ylabel("Publications count")
-    plt.xlabel("Cited by accumulative")
+    # Convert dataPlot to np array
+    dataPlot = np.array(dataPlot)
+
+    print("dataPlot")
+    print dataPlot
+    graphUtils.labeled_scatter_plot(dataPlot, legendArray, plt)
+
+    plt.ylabel("Average growth rate (papers/year)")
+    plt.xlabel("Total cited by")
 
     if args.yLog:
       plt.yscale('log')
@@ -230,7 +316,7 @@ if args.noPlot:
     count = 0
     legendArray=[]
     for topics in topicList:
-      legendArray.append(topics[0])
+      legendArray.append(topicResults[topics[0].upper()]["name"])
 
       plt.plot(topicResults[topics[0].upper()]["year"], topicResults[topics[0].upper()]["PapersCount"],
       linewidth=1.2, marker=globalVar.MARKERS[count], markersize=10,
