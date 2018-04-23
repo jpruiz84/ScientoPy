8056f68a7cdf56f7a4598eb8ad77d497ceb2e224
Juan Ruiz-Rosero <jpabloruiz@unicauca.edu.co>
2018-04-23 09:43:09 -0500

    Color map tab10 by default

--- globalVar.py
+++ globalVar.py
@@ -1,3 +1,5 @@
+from matplotlib import cm
+
 # Default start and end year
 DEFAULT_START_YEAR = 2000
 DEFAULT_END_YEAR = 2018
@@ -18,6 +20,9 @@ INCLUDED_TYPES = ["Conference Paper", "Article",
 TOP_TREND_SIZE = 200
 TREND_PERIODS = 3
 
+# True to use a predefined color map instead the following list
+USE_COLOR_MAP = True
+
 # Default colors and markers for the graphs
 COLORS = ["#ff0000", "#009900", "#0000ff", "#ee7700", "#8b4513", "#8a2be2", "#000000", "#00bfff", "#b8860b", "#fa8072", "#696969",
 "#ff0000", "#009900", "#0000ff", "#ee7700", "#8b4513", "#8a2be2", "#000000", "#00bfff", "#b8860b", "#fa8072", "#696969",
@@ -29,6 +34,12 @@ COLORS = ["#ff0000", "#009900", "#0000ff", "#ee7700", "#8b4513", "#8a2be2", "#00
 "#ff0000", "#009900", "#0000ff", "#ee7700", "#8b4513", "#8a2be2", "#000000", "#00bfff", "#b8860b", "#fa8072", "#696969",
 "#ff0000", "#009900", "#0000ff", "#ee7700", "#8b4513", "#8a2be2", "#000000", "#00bfff", "#b8860b", "#fa8072", "#696969",
 "#ff0000", "#009900", "#0000ff", "#ee7700", "#8b4513", "#8a2be2", "#000000", "#00bfff", "#b8860b", "#fa8072", "#696969"]
+
+if USE_COLOR_MAP:
+  cmap = cm.get_cmap('tab10')
+  for i in range(0,len(COLORS)):
+    COLORS[i] = cmap(i%10)
+
 
 MARKERS = ('o', '^', 's', 'p', '*', 'd', 'D', '8', 'v', '>', 'v', '^', '<', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 
 'o', '^', 's', 'p', '*', 'd', 'D', '8', 'v', '>', 'v', '^', '<', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 
