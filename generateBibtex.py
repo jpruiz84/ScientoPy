c9299fb9964c9a0e7b2a86727510c4ff6672ea76
Juan Ruiz-Rosero <jpabloruiz@unicauca.edu.co>
2018-10-10 08:43:55 -0500

    generateBibtext avoid duplications bib items

--- generateBibtex.py
+++ generateBibtex.py
@@ -43,37 +43,37 @@ bodytext = rawtext[rawtext.find(start)+len(start):rawtext.rfind(end)]
 
 
 # Extact the cites keys
-citesList = []
+citesDict = {}
 for char in range(0,len(bodytext) - 10):
   if bodytext[char:char+6] == '\\cite{':
-    author = ''
+    cite = ''
     char += len('\\cite{')
     while (bodytext[char] != '}'):
       if (bodytext[char] == ' '):
-        char+=1
+        char +=1
       elif(bodytext[char] == ','):
         char +=1 
-        if author in citesList:
-          author = ''
+        if cite in citesDict.keys():
+          cite = ''
         else:
-          citesList.append(author)
-          author=''
+          citesDict[cite] = False
+          cite=''
       else:
-        author += (bodytext[char])
+        cite += (bodytext[char])
         char +=1  
-    if author in citesList:
+    if cite in citesDict.keys():
       pass
     else:
-      citesList.append(author) 
+      citesDict[cite] = False
 
-print(citesList)
+print("%d cites found." % len(citesDict))
+print(citesDict)
 
 # Start paper list empty
 papersDict = []
 papersToBib = []
 
 # Open the storage database and add to papersDict
-#INPUT_FILE = os.path.join(globalVar.RESULTS_FOLDER, globalVar.OUTPUT_FILE_NAME)
 INPUT_FILE = os.path.join(globalVar.DATA_OUT_FOLDER, globalVar.OUTPUT_FILE_NAME)
 ifile = open(INPUT_FILE, "r", encoding='utf-8')
 print("Reading file: %s" % (INPUT_FILE))
@@ -87,34 +87,20 @@ count = 1
 for paper in papersDict:
   #print("%d, %s" % (count, paper["title"]))
   #count += 1
-  if paper["eid"] in citesList:
-    print("******* Added paper: %s" % paper["title"])
-    papersToBib.append(paper)
+  if paper["eid"] in citesDict.keys():
+    if citesDict[paper["eid"]] == False:
+      print("******* Added paper: %s" % paper["title"])
+      papersToBib.append(paper)
+      citesDict[paper["eid"]] = True
 
 
 OUT_FILE = os.path.join(globalVar.RESULTS_FOLDER, globalVar.OUTPUT_FILE_BIB)
 ofile = open(OUT_FILE, 'w', encoding='utf-8')
 
-'''
-@Article{Abril07,
-  author 	= "Patricia S. Abril and Robert Plant",
-  title 	= "The patent holder's dilemma: Buy, sell, or troll?",
-  journal = "Communications of the ACM",
-  volume 	= "50",
-  number 	= "1",
-  month 	= jan,
-  year 		= "2007",
-  pages 	= "36--44",
-  doi 		= "10.1145/1188913.1188915",
-  note		= "",
-}
-
-'''
-
 for paper in papersToBib:
   if(paper["documentType"] in ["Article", "Review", "Article in Press"]):
     ofile.write('@Article{%s,\n' % paper["eid"])
-    ofile.write('  author \t=\t"%s",\n' % paper["authorFull"].replace("; ", " and "))
+    ofile.write('  author \t=\t"%s",\n' % paper["authorFull"].replace("; "," and ").replace(", "," and "))
     ofile.write('  title\t\t=\t"%s",\n' % paper["title"].replace("&", "\&"))
     ofile.write('  journal \t=\t"%s",\n' % paper["sourceTitle"])
     ofile.write('  numpages\t=\t"%s",\n' % paper["pageCount"])
@@ -126,7 +112,7 @@ for paper in papersToBib:
 
   if (paper["documentType"] in ["Conference Paper", "Proceedings Paper",]):
     ofile.write('@Inproceedings{%s,\n'% paper["eid"])
-    ofile.write('  author \t=\t"%s",\n' % paper["authorFull"].replace("; "," and "))
+    ofile.write('  author \t=\t"%s",\n' % paper["authorFull"].replace("; "," and ").replace(", "," and "))
     ofile.write('  title\t\t=\t"%s",\n' % paper["title"].replace("&","\&"))
     ofile.write('  booktitle \t=\t"%s",\n' % paper["sourceTitle"])
     ofile.write('  publisher \t=\t"%s",\n' % paper["publisher"])
