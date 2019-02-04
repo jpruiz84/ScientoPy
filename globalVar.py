c448233e11f91d3b25ebf3e5ebbc1f5e02803221
Juan Ruiz-Rosero <jpabloruiz@unicauca.edu.co>
2019-02-04 08:10:58 -0500

    Defautl start year 1990 and end year 2019

--- globalVar.py
+++ globalVar.py
@@ -28,8 +28,8 @@ if sys.version_info[0] < 3:
   exit()
 
 # Default start and end year
-DEFAULT_START_YEAR = 2000
-DEFAULT_END_YEAR = 2018
+DEFAULT_START_YEAR = 1990
+DEFAULT_END_YEAR = 2019
 
 # Default plot width and height in inches
 DEFAULT_PLOT_WIDTH = 6.4
