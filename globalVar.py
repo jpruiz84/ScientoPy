6696b1c79e5f1ca482b2f0b22db93efd28cabceb
Juan P. Ruiz <juan.ruiz@taiosystems.com>
2020-12-29 13:48:06 -0500

    Added mising -1

--- globalVar.py
+++ globalVar.py
@@ -30,7 +30,7 @@ if sys.version_info[0] < 3:
 
 # Default start and end year
 DEFAULT_START_YEAR = 1990
-DEFAULT_END_YEAR = datetime.datetime.now().year
+DEFAULT_END_YEAR = datetime.datetime.now().year - 1
 
 # Default plot width and height in inches
 DEFAULT_PLOT_WIDTH = 6.4
